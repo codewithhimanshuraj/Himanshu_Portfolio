cf2f2c729aa26ab7ef6e597822d5e5958e93a3f2
codewithhimanshuraj <himanshuraj1707@gmail.com>
2026-08-01 18:19:12 +0530

    Use Gmail service

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -42,11 +42,8 @@ const protect = async (req, res, next) => {
 // Nodemailer
 // =====================
 
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
